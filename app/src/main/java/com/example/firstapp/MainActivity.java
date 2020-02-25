c63de53d820cffca7a98e3d61cc0d9cb756d6dcb
CATHY <ayebarecathie18@gmail.com>
2020-02-25 16:43:03 +0300

    welcome app

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,19 +1,22 @@
 package com.example.firstapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //declaring variables
     MediaPlayer mPlayer;
-   Button buttonplay;
+    Button buttonplay;
     Button buttonpause;
 
 
@@ -21,41 +24,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonplay =(Button) findViewById(R.id.button);
+        //calling variables inside the main method
+        buttonplay = (Button) findViewById(R.id.button);
         buttonplay.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
             public void onClick(View v) {
 
-                mPlayer = MediaPlayer.create(getApplicationContext(),R.raw.omusheshe);
+                mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.omusheshe);
+                if (mPlayer != null && mPlayer.isPlaying()) {
+                    mPlayer.stop();
+                }
+                mPlayer.start();
 
 
             }
         });
-        buttonpause =(Button) findViewById(R.id.button2);
+        buttonpause = (Button) findViewById(R.id.button2);
         buttonpause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                mPlayer.stop();
             }
         });
 
     }
-    public void sendMessage (View view) {
-        EditText message = (EditText)findViewById(R.id.message);
-        //Toast.makeText (this, "Sending message " + message.getText().toString(), Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        intent.putExtra("MESSAGE", message.getText().toString());
-        message.setText("");
-        startActivity(intent);
+        public void sendMessage (View view){
+            EditText message = (EditText) findViewById(R.id.message);
+            //Toast.makeText (this, "Sending DisplayMessageActivity.class);
+            Intent intent = new Intent(MainActivity.this,DisplayMessageActivity.class);
+            intent.putExtra("MESSAGE", message.getText().toString());
+            message.setText("");
+            startActivity(intent);
+    }
+    // this is where menus are inflated
 
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.ayeba,menu);
+        return true;
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemx=item.getItemId();
+        switch (itemx){
+            case R.id.mbarara:
+           Intent myintent1=new Intent(MainActivity.this,ayebare.class);
+           startActivity(myintent1);
+                Toast.makeText(this, "Mbarara is selected", Toast.LENGTH_SHORT).show();
+                return  true;
+            case R.id.makerere:
+                Intent myintent2=new Intent(MainActivity.this,DisplayMessageActivity.class);
+                startActivity(myintent2);
+                Toast.makeText(this, "Makerere is selected", Toast.LENGTH_SHORT).show();
+                return true;
 
-        return super.onCreateOptionsMenu(menu);
+                default:
+
+                    return true;
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/firstapp/ayebare.java
+++ app/src/main/java/com/example/firstapp/ayebare.java
@@ -2,15 +2,21 @@ package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ayebare extends AppCompatActivity {
     MediaPlayer mPlayer;
     Button buttonPlay;
     Button buttonpause;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +25,22 @@ public class ayebare extends AppCompatActivity {
 
         buttonPlay =(Button) findViewById(R.id.button);
         //buttonPlay.setOnClickListener(getApplicationContext(),R.raw.omusheshe);
-        buttonPlay.setOnClickListener(new View.OnClickListener() {
+      /*  buttonPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mPlayer !=null && mPlayer.isPlaying())
-                mPlayer.stop();
+
             }
-        });
+        });*/
+      textView=findViewById(R.id.text1);
+        AssetManager  assetManager=getAssets();
+        InputStream files = null;
+        {
+            try{
+                files = assetManager.open("cathy.txt");
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            textView.setText((CharSequence) files);
+        }
     }
 }
